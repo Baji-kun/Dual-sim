5ed5d1e97751562657152940c1345442d125e32c
Siam19395 <144642933+Siam19395@users.noreply.github.com>
2023-11-19 04:43:08 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,5 +1,3 @@
-#(©)Codexbotz
-#Recoded By @Its_Tartaglia_Childe
 
 from pyrogram import __version__
 from bot import Bot
@@ -11,12 +9,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n┃ Dҽʋҽʅσρҽɾ : <a href='tg://user?id={5205293211}'>ησzєℓ ѕιℓνα #𝕲𝖔𝖉𝕺𝖋𝕮𝖗𝖆𝖈𝖐𝖊𝖗𝖘 </a>\n┃ ¢яєαтσя : <a href='tg://user?id={OWNER_ID}'> тнιѕ ℓєgєη∂ вσу </a>\n┃ Lαɳɠυαɠҽ : <code>Python3</code>\n┃ LιႦɾαɾყ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┃ ѕσυя¢є ¢σ∂є : <a href=https://t.me/Its_Tartaglia_Childe>тαℓк тσ нιм</a>\n┃ мαιη ¢нαηηєℓ : <a href=https://t.me/Anime_X_Hunters>​Anime X Hunters​</a>\n┃ Sυρρσɾƚ Gɾσυρ : <a href=https://t.me/Hunters_Discussion>ѕυρρσят gяσυρ</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
+            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n┃ Developer : <a href='tg://user?id={5971676967}'>Siam Chowdhury #𝕲𝖔𝖉𝕺𝖋𝕮𝖗𝖆𝖈𝖐𝖊𝖗𝖘 </a>\n┃ Creator : <a href='tg://user?id={OWNER_ID}'> This Person </a>\n┃ Language : <code>Python3</code>\n┃ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┃ Source Code : <a href=https://t.me/Chowdhury_Siam>Contact Me</a>\n┃ Main Channel : <a href=https://t.me/Anime_Kun_Channel>​Anime Kun</a>\n┃ Chat Group : <a href=https://t.me/AnimeKunChannel>Anime Kun GC</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("☠️ Close ☠️", callback_data = "close")
+                        InlineKeyboardButton("Close Me", callback_data = "close")
                     ]
                 ]
             )
