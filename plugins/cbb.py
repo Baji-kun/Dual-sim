b87d10e98d83aa0d32c3c9643a8fdb9bdd8987bb
Siam19395 <144642933+Siam19395@users.noreply.github.com>
2023-11-19 05:54:28 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,7 +9,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n┃ Developer : <a href='tg://user?id={5971676967}'>Siam Chowdhury #𝕲𝖔𝖉𝕺𝖋𝕮𝖗𝖆𝖈𝖐𝖊𝖗𝖘 </a>\n┃ Creator : <a href='tg://user?id={OWNER_ID}'> This Person </a>\n┃ Language : <code>Python3</code>\n┃ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┃ Source Code : <a href=https://t.me/Chowdhury_Siam>Contact Me</a>\n┃ Main Channel : <a href=https://t.me/Anime_Kun_Channel>​Anime Kun</a>\n┃ Chat Group : <a href=https://t.me/AnimeKunChannel>Anime Kun GC</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
+            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n┃ Developer : <a href='tg://user?id={5971676967}'>Siam Chowdhury</a>\n┃ Creator : <a href='tg://user?id={OWNER_ID}'> This Person </a>\n┃ Language : <code>Python3</code>\n┃ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┃ Source Code : <a href=https://t.me/Chowdhury_Siam>Contact Me</a>\n┃ Main Channel : <a href=https://t.me/Anime_Kun_Channel>​Anime Kun</a>\n┃ Chat Group : <a href=https://t.me/AnimeKunChannel>Anime Kun GC</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
