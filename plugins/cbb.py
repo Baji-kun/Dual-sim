bde756e35097da537c47bdee7e8e7280fdbb24cf
Zeno79 <144168045+Zeno79@users.noreply.github.com>
2024-01-17 08:14:35 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,7 +9,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n┃ Developer : <a href='https://t.me/Chowdhury_Siam'>Siam Chowdhury</a>\n┃ Creator : <a href='https://t.me/Chowdhury_Siam'> This Person </a>\n┃ Language : <code>Python3</code>\n┃ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┃ Source Code : <a href=https://t.me/Chowdhury_Siam>Contact Me</a>\n┃ Main Channel : <a href=https://t.me/Anime_Kun_Channel>​Anime Kun</a>\n┃ Chat Group : <a href=https://t.me/AnimeKunChannel>Anime Kun GC</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
+            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n┃ Developer : <a href='https://t.me/Itz_Zeno'>Zeno</a>\n┃ Creator : <a href='https://t.me/Itz_Zeno'> This Person </a>\n┃ Language : <code>Python3</code>\n┃ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┃ Source Code : <a href=https://t.me/Netflix_Dual>Movie Channel</a>\n┃ Main Channel : <a href=https://t.me/Anime_Wide>​Anime Wide</a>\n┃ Chat Group : <a href=https://t.me/Anime_Request_Grp>Anime Wide GC</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
