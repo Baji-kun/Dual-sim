0039c045720cdb22a5ee403f80f9e29f20bb2a78
Zeno79 <144168045+Zeno79@users.noreply.github.com>
2024-01-17 21:22:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", ""))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first} {last}\n\nMy name is Miku Nakano.\nI'm a File Share Bot of @Chowdhury_Siam. If you want to use this me too. Message my master.")
+START_MSG = os.environ.get("START_MESSAGE", "Thanks For Using Me ❣️")
 try:
     ADMINS=[6376328008]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -41,7 +41,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}! You Need To Join In My Channel To Access My Files.\n\nKindly Please join My Channel.")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}! ᴊᴏɪɴ ᴛʜᴇ ꜰᴏʟʟᴏᴡɪɴɢ ᴄʜᴀɴɴᴇʟꜱ ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ "ᴛʀʏ ᴀɢᴀɪɴ" ᴀꜰᴛᴇʀ ᴛʜᴀᴛ ʏᴏᴜ ᴡɪʟʟ ʀᴇᴄᴇɪᴠᴇ ʏᴏᴜʀ ꜰɪʟᴇꜱ\n\nKindly ᴊᴏɪɴ ↓")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -53,7 +53,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'false'
 
 BOT_STATS_TEXT = "<b>I'm Alive For</b>\n{uptime}"
-USER_REPLY_TEXT = "Please Don't Message Me, I only Work for My Master @Chowdhury_Siam..!"
+USER_REPLY_TEXT = "Please Don't Message Me, I only Work for My Master @Itz_Zeno..!"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6376328008)
