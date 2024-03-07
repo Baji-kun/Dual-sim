0dd9eae3c7c97861e5a92494da908926f83126c8
Zeno79 <144168045+Zeno79@users.noreply.github.com>
2024-03-08 03:46:42 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,19 +1,17 @@
+import asyncio
 from bot import Bot
 from pyrogram.types import Message
 from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
-from datetime import datetime
-from helper_func import get_readable_time
+from datetime import datetime, timedelta
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
-    now = datetime.now()
-    delta = now - bot.uptime
-    time = get_readable_time(delta.seconds)
-    await message.reply(BOT_STATS_TEXT.format(uptime=time))
+    uptime = get_readable_uptime(bot.uptime)
+    await message.reply(BOT_STATS_TEXT.format(uptime=uptime))
 
-
-@Bot.on_message(filters.private & filters.incoming)
-async def useless(_,message: Message):
+@Bot.on_message(filters.private)
+async def reply_to_private_messages(_, message: Message):
     if USER_REPLY_TEXT:
         await message.reply(USER_REPLY_TEXT)
+        await asyncio.sleep(30)
